Root3287/cap-segw

commit ca9758d7f501a1349b60ea86aea8fe732798e643

mvc: MPC Entity Name Warning
MPC will now give a warning due to limitations in ABAP.

The generators job should be generate based on what it is given.
By passing the warning back to the MPC/DPC we can add annotations.

This also introduce a new annotation `@segw.mpc.define.name`.
The annotatation will allow developers control how the define
method is named.

src/generator/ModelProviderClassGeneratorV2.ts
@@ -11,7 +11,9 @@ import {
 import { Primitive as CDSPrimitive } from "../types/cds";
 import CodeWriter from "./CodeWriter";
 
-import { entity, csn } from "@sap/cds";
+import cds, { entity } from "@sap/cds";
+
+const LOG = cds.log("segw");
 
 export default class ModelProviderClassGeneratorV2 implements IFCodeGenerator, IFServiceClassGenerator {
 	private _class: ABAPClass = { 
@@ -118,10 +120,20 @@ export default class ModelProviderClassGeneratorV2 implements IFCodeGenerator, I
 	public addEntity(entity: entity): void {
 		let splitNamespace = entity.name.split(".");
 		let entityName = (<any>entity)?.["@segw.name"] ?? splitNamespace[splitNamespace.length-1];
+
+		if(entityName.length > 128){
+			LOG.warn(`${entityName} too long. Consider shortening it with @segw.name`);
+		}
+		
+		const methodName = (<any>entity?.["@segw.mpc.define.name"]) ?? `define_${entityName}`;
+		
+		if(methodName > 30){
+			LOG.warn(`Method ${methodName} too long. Consider shortening it with @segw.mpc.define.name`);
+		}
 		
 		let defineEntityMethod: ABAPMethod = {
 			type: ABAPMethodType.MEMBER,
-			name: `define_${entityName}`,
+			name: methodName,
 			raising: [ "/iwbep/cx_mgw_med_exception"]
 		};
 
@@ -142,37 +154,64 @@ export default class ModelProviderClassGeneratorV2 implements IFCodeGenerator, I
 		writer.writeLine(`is_def_entity_set = abap_false`);
 		writer.decreaseIndent().writeLine(").").writeLine();
 
-		// Loop over properties
-		writer.writeLine("property = entity_type->create_property(").increaseIndent();
-		writer.writeLine("iv_property_name = ''");
-		writer.writeLine("iv_abap_fieldname = ''");
-		writer.decreaseIndent().writeLine(").");
-		// writer.writeLine("property->set_is_key( ).");
-		
-		// Types
-		// writer.writeLine("property->set_type_edm_datetime( ).");
-		// writer.writeLine("property->set_type_edm_string( ).");
-		// writer.writeLine("property->set_type_edm_boolean( ).");
-		// writer.writeLine("property->set_type_edm_decimal( ).");
-		
-		// Main OData Annotations
-		// writer.writeLine("property->set_precison( iv_precision = ).");
-		// writer.writeLine("property->set_maxlength( iv_max_length = ).");
-		// writer.writeLine("property->set_creatable( abap_false ).");
-		// writer.writeLine("property->set_updatable( abap_false ).");
-		// writer.writeLine("property->set_nullable( abap_false ).");
-		// writer.writeLine("property->set_sortable( abap_true ).");
-		// writer.writeLine("property->set_filterable( abap_false ).");
-		
-		// TODO: Annotation
-		// writer.writeLine("property->/iwbep/if_mgw_odata_annotatabl~create_annotation( 'sap' )->add(").increaseIndent();
-	    // writer.writeLine("EXPORTING").increaseIndent();
-        // writer.writeLine("iv_key = 'unicode'");
-        // writer.writeLine("iv_value = 'false'");
-        // writer.decreaseIndent().writeLine(").");
+		for(let property of entity.elements){
+			// Loop over properties
+			writer.writeLine("property = entity_type->create_property(").increaseIndent();
+			writer.writeLine("iv_property_name = ''");
+			writer.writeLine("iv_abap_fieldname = ''");
+			writer.decreaseIndent().writeLine(").");
+			
+			if(property?.key)
+				writer.writeLine("property->set_is_key( ).");
+			
+			// const PrimativeLookup: Record<CDSPrimitive, (writer: CodeWriter) => void> = {
+			// 	[CDSPrimitive.UUID]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_guid( )."); },
+			// 	[CDSPrimitive.Boolean]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_boolean( )."); },
+			// 	[CDSPrimitive.Integer]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int32( )."); },
+			// 	[CDSPrimitive.Int16]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int16( )."); },
+			// 	[CDSPrimitive.Int32]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int32( )."); },
+			// 	[CDSPrimitive.Int64]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int64( )."); },
+			// 	[CDSPrimitive.UInt8]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_byte( )."); },
+			// 	[CDSPrimitive.Decimal]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_decimal( )."); },
+			// 	[CDSPrimitive.Double]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_double( )."); },
+			// 	[CDSPrimitive.Date]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_date( )."); },
+			// 	[CDSPrimitive.Time]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_time( )."); },
+			// 	[CDSPrimitive.DateTime]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_datetime( )."); },
+			// 	[CDSPrimitive.Timestamp]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_timestamp( )."); },
+			// 	[CDSPrimitive.String]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_string( )."); },
+			// 	[CDSPrimitive.Binary]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_binary( )."); },
+			// 	[CDSPrimitive.LargeBinary]: (writer: CodeWriter) => { writer.writeLine("property->set_type_edm_binary( )."); },
+			// 	[CDSPrimitive.LargeString]: (writer: CodeWriter) => { writer.writeLine("property->set_type_edm_string( )."); },
+			// 	[CDSPrimitive.Map]: 		(writer: CodeWriter) => { /* NOT SUPPORTED */ },
+			// 	[CDSPrimitive.Vector]: 		(writer: CodeWriter) => { /* NOT SUPPORTED */ },
+			// };
+			// PrimativeLookup[<CDSPrimitive>property.type](writer);
+			
+			// Main OData Annotations
+			
+			// writer.writeLine("property->set_precison( iv_precision = ).");
+			if((<any>property)?.length)
+				writer.writeLine(`property->set_maxlength( iv_max_length = ${(<any>property)?.length} ).`);
+			
+			writer.writeLine("property->set_creatable( abap_false ).");
+			writer.writeLine("property->set_updatable( abap_false ).");
+			writer.writeLine("property->set_nullable( abap_false ).");
+			
+			// if(property?.sortable)
+			// 	writer.writeLine("property->set_sortable( abap_true ).");
+			// if(property?.filterable)
+			// 	writer.writeLine("property->set_filterable( abap_false ).");
+			
+			// TODO: Annotation
+			// writer.writeLine("property->/iwbep/if_mgw_odata_annotatabl~create_annotation( 'sap' )->add(").increaseIndent();
+			// writer.writeLine("EXPORTING").increaseIndent();
+			// writer.writeLine("iv_key = 'unicode'");
+			// writer.writeLine("iv_value = 'false'");
+			// writer.decreaseIndent().writeLine(").");
 
-        // TODO: Labels
-        // lo_property->set_label_from_text_element( iv_text_element_symbol = '033' iv_text_element_container = gc_incl_name ). 
+			// TODO: Labels
+			// lo_property->set_label_from_text_element( iv_text_element_symbol = '033' iv_text_element_container = gc_incl_name ). 			
+		}
 		
 		writer.writeLine();
 		writer.writeLine("entity_set = entity_type->create_entity_set( '' ).").writeLine();

src/generator/ModelProviderClassGeneratorV4.ts
@@ -10,7 +10,9 @@ import {
 } from "../types/abap";
 import CodeWriter from "./CodeWriter";
 
-import { entity } from "@sap/cds";
+import cds, { entity } from "@sap/cds";
+
+const LOG = cds.log("segw");
 
 export default class ModelProviderClassGeneratorV4 implements IFCodeGenerator, IFServiceClassGenerator {
 	private _class: ABAPClass = { 
@@ -55,10 +57,20 @@ export default class ModelProviderClassGeneratorV4 implements IFCodeGenerator, I
 	public addEntity(entity: entity): void {
 		let splitNamespace = entity.name.split(".");
 		let entityName = (<any>entity)?.["@segw.name"] ?? splitNamespace[splitNamespace.length-1];
+
+		if(entityName.length > 128){
+			LOG.warn(`${entityName} too long. Consider shortening it with @segw.name`);
+		}
 		
+		const methodName = (<any>entity?.["@segw.mpc.define.name"]) ?? `define_${entityName}`;
+		
+		if(methodName > 30){
+			LOG.warn(`Method ${methodName} too long. Consider shortening it with @segw.mpc.define.name`);
+		}
+
 		let defineEntityMethod: ABAPMethod = {
 			type: ABAPMethodType.MEMBER,
-			name: `define_${entityName}`,
+			name: methodName,
 			importing: [
 				{
 					name: "io_model",

src/generator/ABAPGenerator.ts
@@ -132,7 +132,7 @@ export default class ABAPGenerator implements IFCodeGenerator {
 	private _writeMethodDefinition(method: ABAP.Method): void {
 		this._writer.increaseIndent();
 
-		let methodDefinition = `${method.type}S ${method.name.substring(0,30)}`;
+		let methodDefinition = `${method.type}S ${method.name}`;
 		if(method.isRedefinition) methodDefinition += " REDEFINITION.";
 		if(method.isFinal) methodDefinition += " FINAL";
 		this._writer.writeLine(methodDefinition);
@@ -216,7 +216,7 @@ export default class ABAPGenerator implements IFCodeGenerator {
 	 * @param {ABAPMethod} method Method to write
 	 */
 	private _writeMethodImplementation(method: ABAP.Method): void {
-		this._writer.writeLine(`METHOD ${method.name.substring(0,30)}.`).increaseIndent();
+		this._writer.writeLine(`METHOD ${method.name}.`).increaseIndent();
 		method?.code?.forEach((c) => {
 			this._writer.writeLine(c);
 		})

src/generator/DataProviderClassGeneratorV2.ts
@@ -11,7 +11,9 @@ import {
 } from "../types/abap";
 import CodeWriter from "./CodeWriter";
 
-import { entity } from "@sap/cds";
+import cds, { entity } from "@sap/cds";
+
+const LOG = cds.log("segw");
 
 export default class DataProviderClassGeneratorV2 implements IFCodeGenerator, IFServiceClassGenerator {
 	private _class: ABAPClass = { 
@@ -69,6 +71,10 @@ export default class DataProviderClassGeneratorV2 implements IFCodeGenerator, IF
 		let splitNamespace = entity.name.split(".");
 		let entityName = (<any>entity)?.["@segw.name"] ?? splitNamespace[splitNamespace.length-1];
 		let entityReturnType = `ZCL_MPC=>T_${entityName}`;
+		
+		if(entityName.length > 30){
+			LOG.warn(`Method ${entityName} too long. Consider shortening it with @segw.name`);
+		}
 
 		this._createEntity(entityName, entityReturnType);
 		this._deleteEntity(entityName, entityReturnType);

test/MPCV4.test.ts
@@ -1,6 +1,7 @@
 import ModelProviderClassGeneratorV4 from "../src/generator/ModelProviderClassGeneratorV4";
 
-import { entity } from "@sap/cds";
+import cds, { entity } from "@sap/cds";
+const LOG = cds.log("segw");
 
 describe("MVC-V4", () => {
 	let mpc: ABAPGenerator;
@@ -25,5 +26,17 @@ describe("MVC-V4", () => {
 		const code = mpc.generate();
 		console.log(code);
 		expect(code).not.toBeNull();
+	});
+
+	test("Entity Name Too Long", () => {
+		const logSpy = jest.spyOn(LOG, 'warn');
+		let entity: entity = {
+			name: "A".repeat(140),
+		};
+		mpc.addEntity(entity);
+
+		const code = mpc.generate();
+		expect(code).not.toBeNull();
+		expect(logSpy).toHaveBeenCalled();
 	});
 });

commit 40edd7071f35420404c9043af2154e1f3c46bfd0

feat: MPC-V2 Annotations
This commit adds proper types and annotations to the MPC-V2.

Some of the annotations that SAP CAP has doesn't really match
to SEGW, so we need to add additional custom annotations.

Added Annotations:
	- @segw.sortable
	- @segw.filterable
	- @segw.pageable
	- @segw.ftxt_search
	- @segw.subscrible
	- @segw.filter_required

This commit also fixes typescript errors introduced from commit
ca9758d7f501a1349b60ea86aea8fe732798e643.

src/generator/ModelProviderClassGeneratorV2.ts
@@ -125,7 +125,7 @@ export default class ModelProviderClassGeneratorV2 implements IFCodeGenerator, I
 			LOG.warn(`${entityName} too long. Consider shortening it with @segw.name`);
 		}
 		
-		const methodName = (<any>entity?.["@segw.mpc.define.name"]) ?? `define_${entityName}`;
+		const methodName = ((<any>entity)?.["@segw.mpc.define.name"]) ?? `define_${entityName}`;
 		
 		if(methodName > 30){
 			LOG.warn(`Method ${methodName} too long. Consider shortening it with @segw.mpc.define.name`);
@@ -164,43 +164,86 @@ export default class ModelProviderClassGeneratorV2 implements IFCodeGenerator, I
 			if(property?.key)
 				writer.writeLine("property->set_is_key( ).");
 			
-			// const PrimativeLookup: Record<CDSPrimitive, (writer: CodeWriter) => void> = {
-			// 	[CDSPrimitive.UUID]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_guid( )."); },
-			// 	[CDSPrimitive.Boolean]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_boolean( )."); },
-			// 	[CDSPrimitive.Integer]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int32( )."); },
-			// 	[CDSPrimitive.Int16]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int16( )."); },
-			// 	[CDSPrimitive.Int32]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int32( )."); },
-			// 	[CDSPrimitive.Int64]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_int64( )."); },
-			// 	[CDSPrimitive.UInt8]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_byte( )."); },
-			// 	[CDSPrimitive.Decimal]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_decimal( )."); },
-			// 	[CDSPrimitive.Double]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_double( )."); },
-			// 	[CDSPrimitive.Date]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_date( )."); },
-			// 	[CDSPrimitive.Time]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_time( )."); },
-			// 	[CDSPrimitive.DateTime]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_datetime( )."); },
-			// 	[CDSPrimitive.Timestamp]: 	(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_timestamp( )."); },
-			// 	[CDSPrimitive.String]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_string( )."); },
-			// 	[CDSPrimitive.Binary]: 		(writer: CodeWriter) => { writer.writeLine("property->set_type_edm_binary( )."); },
-			// 	[CDSPrimitive.LargeBinary]: (writer: CodeWriter) => { writer.writeLine("property->set_type_edm_binary( )."); },
-			// 	[CDSPrimitive.LargeString]: (writer: CodeWriter) => { writer.writeLine("property->set_type_edm_string( )."); },
-			// 	[CDSPrimitive.Map]: 		(writer: CodeWriter) => { /* NOT SUPPORTED */ },
-			// 	[CDSPrimitive.Vector]: 		(writer: CodeWriter) => { /* NOT SUPPORTED */ },
-			// };
-			// PrimativeLookup[<CDSPrimitive>property.type](writer);
+			switch(property.type){
+				case CDSPrimitive.UUID:
+					writer.writeLine("property->set_type_edm_guid( ).");
+					break;
+				case CDSPrimitive.Boolean:
+					writer.writeLine("property->set_type_edm_boolean( ).");
+					break;
+				case CDSPrimitive.Integer:
+					writer.writeLine("property->set_type_edm_int32( ).");
+					break;
+				case CDSPrimitive.Int16:
+					writer.writeLine("property->set_type_edm_int16( ).");
+					break;
+				case CDSPrimitive.Int32:
+					writer.writeLine("property->set_type_edm_int32( ).");
+					break;
+				case CDSPrimitive.Int64:
+					writer.writeLine("property->set_type_edm_int64( ).");
+					break;
+				case CDSPrimitive.UInt8:
+					writer.writeLine("property->set_type_edm_byte( ).");
+					break;
+				case CDSPrimitive.Decimal:
+					writer.writeLine("property->set_type_edm_decimal( ).");
+					break;
+				case CDSPrimitive.Double:
+					writer.writeLine("property->set_type_edm_double( ).");
+					break;
+				case CDSPrimitive.Date:
+					writer.writeLine("property->set_type_edm_date( ).");
+					break;
+				case CDSPrimitive.Time:
+					writer.writeLine("property->set_type_edm_time( ).");
+					break;
+				case CDSPrimitive.DateTime:
+					writer.writeLine("property->set_type_edm_datetime( ).");
+					break;
+				case CDSPrimitive.Timestamp:
+					writer.writeLine("property->set_type_edm_timestamp( ).");
+					break;
+				case CDSPrimitive.String:
+					writer.writeLine("property->set_type_edm_string( ).");
+					break;
+				case CDSPrimitive.Binary:
+					writer.writeLine("property->set_type_edm_binary( ).");
+					break;
+				case CDSPrimitive.LargeBinary:
+					writer.writeLine("property->set_type_edm_binary( ).");
+					break;
+				case CDSPrimitive.LargeString:
+					writer.writeLine("property->set_type_edm_string( ).");
+					break;
+				default:
+					break;
+			}
 			
 			// Main OData Annotations
-			
 			// writer.writeLine("property->set_precison( iv_precision = ).");
 			if((<any>property)?.length)
 				writer.writeLine(`property->set_maxlength( iv_max_length = ${(<any>property)?.length} ).`);
 			
-			writer.writeLine("property->set_creatable( abap_false ).");
-			writer.writeLine("property->set_updatable( abap_false ).");
-			writer.writeLine("property->set_nullable( abap_false ).");
+			const readOnly = ((<any>entity)?.["@readonly"] || (<any>property)?.["@readonly"]);
+			let readOnlyAbap = this._toAbapBool(!readOnly);
+			writer.writeLine(`property->set_creatable( ${readOnlyAbap} ).`);
+			writer.writeLine(`property->set_updatable( ${readOnlyAbap} ).`);
 			
-			// if(property?.sortable)
-			// 	writer.writeLine("property->set_sortable( abap_true ).");
-			// if(property?.filterable)
-			// 	writer.writeLine("property->set_filterable( abap_false ).");
+			const manditory = (<any>property)?.["@manditory"] ? "abap_false" : "abap_true";
+			writer.writeLine(`property->set_nullable( ${manditory} ).`);
+			
+			// Documentation is sparse on this one...
+			if((<any>property)?.["@segw.sortable"]){
+				let abapBool = this._toAbapBool((<any>property)?.["@segw.sortable"]);
+				writer.writeLine(`property->set_sortable( ${abapBool} ).`);
+			}
+			
+			// Documentation is sparse on this one..
+			if((<any>property)?.["@segw.filterable"]){
+				let abapBool = this._toAbapBool((<any>property)?.["@segw.filterable"]);
+				writer.writeLine(`property->set_filterable( ${abapBool} ).`);
+			}
 			
 			// TODO: Annotation
 			// writer.writeLine("property->/iwbep/if_mgw_odata_annotatabl~create_annotation( 'sap' )->add(").increaseIndent();
@@ -215,20 +258,36 @@ export default class ModelProviderClassGeneratorV2 implements IFCodeGenerator, I
 		
 		writer.writeLine();
 		writer.writeLine("entity_set = entity_type->create_entity_set( '' ).").writeLine();
-		writer.writeLine("entity_set->set_creatable( abap_false ).");
-		writer.writeLine("entity_set->set_updatable( abap_false ).");
-		writer.writeLine("entity_set->set_deletable( abap_false ).");
+		
+		let readOnlyAbap = this._toAbapBool(!(<any>entity)?.["@readonly"]);
+		writer.writeLine(`entity_set->set_creatable( ${readOnlyAbap} ).`);
+		writer.writeLine(`entity_set->set_updatable( ${readOnlyAbap} ).`);
+		writer.writeLine(`entity_set->set_deletable( ${readOnlyAbap} ).`);
 		writer.writeLine();
-		writer.writeLine("entity_set->set_pageable( abap_true ).");
-		writer.writeLine("entity_set->set_addressable( abap_false ).");
-		writer.writeLine("entity_set->set_has_ftxt_search( abap_false ).");
-		writer.writeLine("entity_set->set_subscribable( abap_false ).");
-		writer.writeLine("entity_set->set_filter_required( abap_false ).");
-
+		
+		if((<any>entity)?.["@segw.pageable"]){
+			writer.writeLine(`entity_set->set_pageable( ${this._toAbapBool((<any>entity)?.["@segw.sortable"])} ).`);
+		}
+		if((<any>entity)?.["@segw.addressable"]){
+			writer.writeLine(`entity_set->set_addressable( ${this._toAbapBool((<any>entity)?.["@segw.addressable"])} ).`);
+		}
+		if((<any>entity)?.["@segw.ftxt_search"]){
+			writer.writeLine(`entity_set->set_has_ftxt_search( ${this._toAbapBool((<any>entity)?.["@segw.ftxt_search"])} ).`);
+		}
+		if((<any>entity)?.["@segw.subscribable"]){
+			writer.writeLine(`entity_set->set_subscribable( ${this._toAbapBool((<any>entity)?.["@segw.subscribable"])} ).`);
+		}
+		if((<any>entity)?.["@segw.filter_required"]){
+			writer.writeLine(`entity_set->set_filter_required( ${this._toAbapBool((<any>entity)?.["@segw.filter_required"])} ).`);
+		}
 		writer.writeLine();
 
 		defineEntityMethod.code = writer.generate().split("\n");
 		this._entityDefineMethods.push(defineEntityMethod.name);
 		this._class?.protectedSection?.methods?.push(defineEntityMethod);
 	};
+
+	private _toAbapBool(value: boolean): string {
+		return (value) ? "abap_true" : "abap_false";
+	}
 }

src/generator/ModelProviderClassGeneratorV4.ts
@@ -62,7 +62,7 @@ export default class ModelProviderClassGeneratorV4 implements IFCodeGenerator, I
 			LOG.warn(`${entityName} too long. Consider shortening it with @segw.name`);
 		}
 		
-		const methodName = (<any>entity?.["@segw.mpc.define.name"]) ?? `define_${entityName}`;
+		const methodName = ((<any>entity)?.["@segw.mpc.define.name"]) ?? `define_${entityName}`;
 		
 		if(methodName > 30){
 			LOG.warn(`Method ${methodName} too long. Consider shortening it with @segw.mpc.define.name`);