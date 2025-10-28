ae15a0b2b27d5516dfab329f64b07b26de31a4d0
Timothy Gibbons <tjgibbons10@gmail.com>
2025-10-28 14:17:57 -0500

    feat(dpc-v2): Handler Function Implmentation

--- src/generator/DataProviderClassGeneratorV2.ts
+++ src/generator/DataProviderClassGeneratorV2.ts
@@ -286,43 +286,304 @@ export default class DataProviderClassGeneratorV2 implements IFServiceClassGener
 		});
 	}
 
-	protected _handleGetEntitySet(){ 
+	protected _handleGetEntitySet(){
+		let writer = new CodeWriter();
+
+		writer.writeLine(`" TODO: Write Type MPC Type Def`).writeLine();
+		writer.writeLine("DATA(entityset_name) = io_tech_request_context->get_entity_set_name( ).");
+		writer.writeLine("DATA empty_entity type ref to data.");
+
+		writer.writeLine("CASE entityset_name.").increaseIndent();
+		
+		for(let entity of [{}]){
+			let entityName = "TEST";
+			writer.writeLine(`WHEN '${entityName}'.`).increaseIndent();
+			writer.writeLine(`me->${entityName}_get_entityset(`).increaseIndent();
+			writer.writeLine("EXPORTING").increaseIndent();
+			writer.writeLine(`entity_name = iv_entity_name`);
+			writer.writeLine(`entityset_name = iv_entity_set_name`);
+			writer.writeLine(`source_name = iv_source_name`);
+			writer.writeLine(`filter_select_options = it_filter_select_options`);
+			writer.writeLine(`order = it_order`);
+			writer.writeLine(`paging = is_paging`);
+			writer.writeLine(`navigation_path = it_navigation_path`);
+			writer.writeLine(`key_tab = it_key_tab`);
+			writer.writeLine(`filter_string = iv_filter_string`);
+			writer.writeLine(`search_string = iv_search_string`);
+			writer.writeLine(`tech_request_context = io_tech_request_context`);
+			writer.decreaseIndent().writeLine("IMPORTING").increaseIndent();
+			writer.writeLine(`response_context = es_response_context`);
+			writer.decreaseIndent().writeLine("RETURNING").increaseIndent();
+			writer.writeLine(`entity_set = data(${entityName}_get_entityset)`);
+			writer.decreaseIndent();
+			writer.decreaseIndent().writeLine(`).`);
+			
+			writer.writeLine();
+			writer.writeLine(`me->copy_data_to_ref(`).increaseIndent();
+			writer.writeLine(`EXPORTING`).increaseIndent();
+			writer.writeLine(`is_data = ${entityName}_get_entityset`);
+			writer.decreaseIndent().writeLine("CHANGING").increaseIndent();
+			writer.writeLine(`cr_data = er_entityset`).decreaseIndent();
+			writer.decreaseIndent().writeLine(`).`).writeLine();
+
+			writer.decreaseIndent();
+		}
+		
+		writer.writeLine("when others.").increaseIndent();
+		writer.writeLine("super->/iwbep/if_mgw_appl_srv_runtime~get_entityset(").increaseIndent();
+		writer.writeLine("EXPORTING").increaseIndent();
+		writer.writeLine(`iv_entity_name = iv_entity_name`);
+		writer.writeLine(`iv_entityset_name = iv_entity_set_name`);
+		writer.writeLine(`iv_source_name = iv_source_name`);
+		writer.writeLine(`it_filter_select_options = it_filter_select_options`);
+		writer.writeLine(`it_order = it_order`);
+		writer.writeLine(`is_paging = is_paging`);
+		writer.writeLine(`it_navigation_path = it_navigation_path`);
+		writer.writeLine(`it_key_tab = it_key_tab`);
+		writer.writeLine(`iv_filter_string = iv_filter_string`);
+		writer.writeLine(`iv_search_string = iv_search_string`);
+		writer.writeLine(`io_tech_request_context = io_tech_request_context`);
+		writer.decreaseIndent().writeLine("IMPORTING").increaseIndent();
+		writer.writeLine("er_entityset = er_entityset");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine(").");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine("ENDCASE.");
+
+		let code = writer.generate().split('\n');
 		this._class?.publicSection?.methods?.push({
 			type: ABAPMethodType.MEMBER,
 			name: "/iwbep/if_mgw_appl_srv_runtime~get_entity_set",
-			isRedefinition: true
+			isRedefinition: true,
+			code: code
 		});
 	}
 
-	protected _handleGetEntity(){ 
+	protected _handleGetEntity(){
+		let writer = new CodeWriter();
+
+		writer.writeLine(`" TODO: Write Type MPC Type Def`).writeLine();
+		writer.writeLine("DATA(entityset_name) = io_tech_request_context->get_entity_set_name( ).");
+		writer.writeLine("DATA empty_entity type ref to data.");
+
+		writer.writeLine("CASE entityset_name.").increaseIndent();
+		
+		for(let entity of [{}]){
+			let entityName = "TEST";
+			writer.writeLine(`WHEN '${entityName}'.`).increaseIndent();
+			writer.writeLine(`me->${entityName}_get_entity(`).increaseIndent();
+			writer.writeLine("EXPORTING").increaseIndent();
+			writer.writeLine(`entity_name = iv_entity_name`);
+			writer.writeLine(`entityset_name = iv_entity_set_name`);
+			writer.writeLine(`source_name = iv_source_name`);
+			writer.writeLine(`data_provider = io_data_provider`);
+			writer.writeLine(`key_tab = it_key_tab`);
+			writer.writeLine(`navigation_path = it_navigation_path`);
+			writer.writeLine(`tech_request_context = io_tech_request_context`);
+			writer.decreaseIndent().writeLine("IMPORTING").increaseIndent();
+			writer.writeLine(`response_context = es_response_context`);
+			writer.decreaseIndent().writeLine("RETURNING").increaseIndent();
+			writer.writeLine(`entity = data(${entityName}_get_entity)`);
+			writer.decreaseIndent();
+			writer.decreaseIndent().writeLine(`).`);
+
+			writer.writeLine();
+			writer.writeLine(`IF ${entityName}_get_entity is initial.`).increaseIndent();
+			writer.writeLine(`er_entity = empty_entity.`);
+			writer.writeLine(`break.`)
+			writer.decreaseIndent().writeLine(`ENDIF.`);
+			
+			writer.writeLine();
+			writer.writeLine(`me->copy_data_to_ref(`).increaseIndent();
+			writer.writeLine(`EXPORTING`).increaseIndent();
+			writer.writeLine(`is_data = ${entityName}_get_entity`);
+			writer.decreaseIndent().writeLine("CHANGING").increaseIndent();
+			writer.writeLine(`cr_data = er_entity`).decreaseIndent();
+			writer.decreaseIndent().writeLine(`).`).writeLine();
+
+			writer.decreaseIndent();
+		}
+		
+		writer.writeLine("when others.").increaseIndent();
+		writer.writeLine("super->/iwbep/if_mgw_appl_srv_runtime~create_entity(").increaseIndent();
+		writer.writeLine("EXPORTING").increaseIndent();
+		writer.writeLine("iv_entity_name = iv_entity_name");
+		writer.writeLine("iv_set_entity_name = iv_entity_set_name");
+		writer.writeLine("iv_source_name = iv_source_name");
+		writer.writeLine("it_key_tab = it_key_tab");
+		writer.writeLine("it_navigation_path = it_navigation_path");
+		writer.decreaseIndent().writeLine("IMPORTING").increaseIndent();
+		writer.writeLine("er_entity = er_entity");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine(").");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine("ENDCASE.");
+
+		let code = writer.generate().split('\n');
 		this._class?.publicSection?.methods?.push({
 			type: ABAPMethodType.MEMBER,
 			name: "/iwbep/if_mgw_appl_srv_runtime~get_entity",
-			isRedefinition: true
+			isRedefinition: true,
+			code: code
 		});
 	}
 
-	protected _handleUpdateEntity(){ 
+	protected _handleUpdateEntity(){
+		let writer = new CodeWriter();
+		writer.writeLine(`" TODO: Write Type MPC Type Def`).writeLine();
+		writer.writeLine("DATA(entityset_name) = io_tech_request_context->get_entity_set_name( ).");
+		writer.writeLine("DATA empty_entity type ref to data.");
+
+		writer.writeLine("CASE entityset_name.").increaseIndent();
+		
+		for(let entity of [{}]){
+			let entityName = "TEST";
+			writer.writeLine(`WHEN '${entityName}'.`).increaseIndent();
+			writer.writeLine(`data(${entityName}_update_entity) = me->${entityName}_update_entity(`).increaseIndent();
+			writer.writeLine(`entity_name = iv_entity_name`);
+			writer.writeLine(`entityset_name = iv_entity_set_name`);
+			writer.writeLine(`source_name = iv_source_name`);
+			writer.writeLine(`data_provider = io_data_provider`);
+			writer.writeLine(`key_tab = it_key_tab`);
+			writer.writeLine(`navigation_path = it_navigation_path`);
+			writer.writeLine(`tech_request_context = io_tech_request_context`);
+			writer.decreaseIndent();
+			writer.writeLine(`).`);
+
+			writer.writeLine();
+			writer.writeLine(`IF ${entityName}_update_entity is initial.`).increaseIndent();
+			writer.writeLine(`er_entity = empty_entity.`);
+			writer.writeLine(`break.`)
+			writer.decreaseIndent().writeLine(`ENDIF.`);
+			
+			writer.writeLine();
+			writer.writeLine(`me->copy_data_to_ref(`).increaseIndent();
+			writer.writeLine(`EXPORTING`).increaseIndent();
+			writer.writeLine(`is_data = ${entityName}_update_entity`);
+			writer.decreaseIndent().writeLine("CHANGING").increaseIndent();
+			writer.writeLine(`cr_data = er_entity`).decreaseIndent();
+			writer.decreaseIndent().writeLine(`).`).writeLine();
+
+			writer.decreaseIndent();
+		}
+		
+		writer.writeLine("when others.").increaseIndent();
+		writer.writeLine("super->/iwbep/if_mgw_appl_srv_runtime~update_entity(").increaseIndent();
+		writer.writeLine("EXPORTING").increaseIndent();
+		writer.writeLine("iv_entity_name = iv_entity_name");
+		writer.writeLine("iv_entity_set_name = iv_entity_set_name");
+		writer.writeLine("iv_source_name = iv_source_name");
+		writer.writeLine("io_data_provider = io_data_provider");
+		writer.writeLine("it_key_tab = it_key_tab");
+		writer.writeLine("it_navigation_path = it_navigation_path");
+		writer.writeLine("io_tech_request_context = io_tech_request_context");
+		writer.decreaseIndent().writeLine("IMPORTING").increaseIndent();
+		writer.writeLine("er_entity = er_entity");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine(").");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine("ENDCASE.");
+
+		let code = writer.generate().split('\n');
 		this._class?.publicSection?.methods?.push({
 			type: ABAPMethodType.MEMBER,
 			name: "/iwbep/if_mgw_appl_srv_runtime~update_entity",
-			isRedefinition: true
+			isRedefinition: true,
+			code: code
 		});
 	}
 
-	protected _handleCreateEntity(){ 
+	protected _handleCreateEntity(){
+
+		let writer = new CodeWriter();
+		writer.writeLine(`" TODO: Write Type MPC Type Def`).writeLine();
+		writer.writeLine("DATA(entityset_name) = io_tech_request_context->get_entity_set_name( ).");
+
+		writer.writeLine("CASE entityset_name.").increaseIndent();
+		
+		for(let entity of [{}]){
+			let entityName = "TEST";
+			writer.writeLine(`WHEN '${entityName}'.`).increaseIndent();
+			writer.writeLine(`data(${entityName}_create_entity) = me->${entityName}_create_entity(`).increaseIndent();
+			writer.writeLine(`entity_name = iv_entity_name`);
+			writer.writeLine(`entity_set_name = iv_entity_set_name`);
+			writer.writeLine(`source_name = iv_source_name`);
+			writer.writeLine(`data_provider = io_data_provider`);
+			writer.writeLine(`key_tab = it_key_tab`);
+			writer.writeLine(`navigation_path = it_navigation_path`);
+			writer.writeLine(`tech_request_context = io_tech_request_context`);
+			writer.decreaseIndent().writeLine(`).`).writeLine();
+			writer.writeLine(`me->copy_data_to_ref(`).increaseIndent();
+			writer.writeLine(`EXPORTING`).increaseIndent();
+			writer.writeLine(`is_data = ${entityName}_create_entity`);
+			writer.decreaseIndent().writeLine("CHANGING").increaseIndent();
+			writer.writeLine(`cr_data = er_entity`).decreaseIndent();
+			writer.decreaseIndent().writeLine(`).`).writeLine();
+			writer.decreaseIndent();
+		}
+		
+		writer.writeLine("when others.").increaseIndent();
+		writer.writeLine("super->/iwbep/if_mgw_appl_srv_runtime~create_entity(").increaseIndent();
+		writer.writeLine("EXPORTING").increaseIndent();
+		writer.writeLine("iv_entity_name = iv_entity_name");
+		writer.writeLine("iv_source_name = iv_source_name");
+		writer.writeLine("io_data_provider = io_data_provider");
+		writer.writeLine("it_key_tab = it_key_tab");
+		writer.writeLine("it_navigation_path = it_navigation_path");
+		writer.decreaseIndent().writeLine("IMPORTING").increaseIndent();
+		writer.writeLine("er_entity = er_entity");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine(").");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine("ENDCASE.");
+
+		let code = writer.generate().split('\n');
 		this._class?.publicSection?.methods?.push({
 			type: ABAPMethodType.MEMBER,
 			name: "/iwbep/if_mgw_appl_srv_runtime~create_entity",
-			isRedefinition: true
+			isRedefinition: true,
+			code: code
 		});
 	}
 
-	protected _handleDeleteEntity(){ 
+	protected _handleDeleteEntity(){
+		let writer = new CodeWriter();
+		writer.writeLine(`" TODO: Write Type MPC Type Def`).writeLine();
+		writer.writeLine("DATA(entityset_name) = io_tech_request_context->get_entity_set_name( ).");
+
+		writer.writeLine("CASE entityset_name.").increaseIndent();
+		
+		for(let entity of [{}]){
+			let entityName = "TEST";
+			writer.writeLine(`WHEN '${entityName}'.`).increaseIndent();
+			writer.writeLine(`me->${entityName}_delete_entity(`).increaseIndent();
+			writer.writeLine(`entity_name = iv_entity_name`);
+			writer.writeLine(`entity_set_name = iv_entity_set_name`);
+			writer.writeLine(`source_name = iv_source_name`);
+			writer.writeLine(`key_tab = it_key_tab`);
+			writer.writeLine(`navigation_path = it_navigation_path`);
+			writer.writeLine(`tech_request_context = io_tech_request_context`);
+			writer.decreaseIndent().writeLine(`).`).writeLine();
+			writer.decreaseIndent();
+		}
+		
+		writer.writeLine("when others.").increaseIndent();
+		writer.writeLine("super->/iwbep/if_mgw_appl_srv_runtime~delete_entity(").increaseIndent();
+		writer.writeLine("iv_entity_name = iv_entity_name");
+		writer.writeLine("iv_entity_set_name = iv_entity_set_name");
+		writer.writeLine("iv_source_name = iv_source_name")
+		writer.writeLine("iv_key_tab = iv_key_tab")
+		writer.writeLine("iv_navigation_path = iv_navigation_path")
+		writer.decreaseIndent().writeLine(").");
+		writer.decreaseIndent();
+		writer.decreaseIndent().writeLine("ENDCASE.");
+
+		let code = writer.generate().split('\n');
 		this._class?.publicSection?.methods?.push({
 			type: ABAPMethodType.MEMBER,
 			name: "/iwbep/if_mgw_appl_srv_runtime~delete_entity",
-			isRedefinition: true
+			isRedefinition: true,
+			code: code
 		});
 	}
 }
