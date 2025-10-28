3f24d008bac343773ad57d2a2cd3fc9a0fe81a4a
Timothy Gibbons <tjgibbons10@gmail.com>
2025-10-27 21:32:50 -0500

    refactor: ABAP Utilities

---
+++ src/utils/ABAP.ts
@@ -0,0 +1,8 @@
+export class ABAP {
+	static getABAPName(definition: any): string {
+		let splitNamespace = definition.name.split(".");
+		let name = (<any>definition)?.["@segw.name"] ?? splitNamespace[splitNamespace.length-1];
+		return name;
+	};
+	static toABAPBool(value: boolean): string { return (value) ? "abap_true" : "abap_false"; }
+}
