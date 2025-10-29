1fbcd416a09f49f54d224b74f01fd92f05ee345f
Timothy Gibbons <tjgibbons10@gmail.com>
2025-10-28 20:05:52 -0500

    refactor: Converting Strings to ABAP Name

--- src/utils/ABAP.ts
+++ src/utils/ABAP.ts
@@ -1,5 +1,8 @@
 export class ABAP {
 	static getABAPName(definition: any): string {
+		if(typeof definition === 'string' || definition instanceof String){
+			return definition.toString().replace(/[\s.]+/g, '_');
+		}
 		let splitNamespace = definition?.name?.split?.(".");
 		let name = (<any>definition)?.["@segw.name"] ?? splitNamespace[splitNamespace.length-1];
 		return name;
